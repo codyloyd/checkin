7aaafbca5d3f1ab06ee78b93b47683e3e00f901a
Cody Loyd <codyloyd@gmail.com>
2017-02-08 10:37:09 -0600

    fetch team details, display on manageTeam

--- pages/manageTeam.js
+++ pages/manageTeam.js
@@ -1,16 +1,32 @@
 import Layout from '../components/layout'
+import {db} from '../lib/firebase'
 
 export default class extends React.Component {
   static async getInitialProps ({ req, query: { id } }) {
-    
     return {id}
-  } 
+  }
+  constructor () {
+    super()
+    this.state = {
+      name: '',
+      owner: ''
+    }
+  }
+  componentDidMount () {
+    db.fetchTeamWithId(this.props.id).then(data => {
+      const {name, owner, joinCode} = data.val()
+      this.setState({name, owner, joinCode})
+    })
+
+  }
   render () {
+    console.log(this.props.id)
     return (
       <Layout>
         <h1>Manage Team</h1>
-        <p>teamID: {this.props.id}</p>
-        <p></p>
+        <p>team name: {this.state.name}</p>
+        <p>team owner: {this.state.owner}</p>
+        <p>member join code: {this.state.joinCode}</p>
       </Layout>
     )
   }
---
+++ spec/firebaseAuthSpec.js
@@ -0,0 +1,18 @@
+import test from 'tape'
+import {db} from '../lib/firebase'
+
+test('fetch single team with Key', assert => {
+  const teamId = '-KcTJlYb35AN3NvaAn10'
+  const expected = {
+    joinCode: 'testing',
+    name: 'test team',
+    owner: 'test owner',
+    members: ['foo', 'bar', 'baz']
+  }
+  db.fetchTeamWithId(teamId).then(data =>{
+    const actual = data.val()
+    assert.deepEqual(actual,expected, 'fetchTeamWithId should fetch a team')
+    assert.end()
+  })
+  .catch(e => console.log(e))
+})
