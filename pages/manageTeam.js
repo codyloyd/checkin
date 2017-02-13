28b2030e8affe1dceb85198c26b2ef9fbd0a09d4
Cody Loyd <codyloyd@gmail.com>
2017-02-13 12:53:10 -0600

    added checkin button to manageTeam

--- pages/manageTeam.js
+++ pages/manageTeam.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import moment from 'moment'
 
 import Layout from '../components/layout'
@@ -31,8 +32,10 @@ export default class extends React.Component {
         <h1 className='blue-text extra-bold'>{this.state.name}</h1>
         <p>team owner: {this.state.owner}</p>
         <p>member join code: {this.state.joinCode}</p>
+        <Link href={`/checkIn?id=${this.props.id}`}><a className='btn green waves-effect waves-light'>CHECK IN</a></Link>
+        
         <h3>Recent Checkins:</h3>
-        {Object.values(this.state.checkins).map(checkin => {
+        {Object.values(this.state.checkins).reverse().map(checkin => {
           return (
             <Checkin key={checkin.time} checkin={checkin} />
           )
