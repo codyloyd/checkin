e98db1b74ef90f4b847ea381279cda98d9acd853
Cody Loyd <codyloyd@gmail.com>
2017-02-14 07:35:31 -0600

    new team redirects to the new team

--- pages/newTeam.js
+++ pages/newTeam.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Router from 'next/router'
 
 import Layout from '../components/layout'
 import {db} from '../lib/firebase'
@@ -7,17 +8,10 @@ import TeamList from '../components/teamlist'
 export default () => (
   <Layout title="New Team">
     <h1>New Team</h1>
-    <h2>Setup a new team here</h2>
     <TeamForm />
-    <p>List of teams (for testing)</p>
-    <TeamList />
   </Layout>
 )
 
-// we can refactor these out into their own files later..
-// for now it's easier if we keep it all together
-// Eric would yell at us for using classes here....
-// I'd like to get around it, but I don't think there is a way with firebase
 class TeamForm extends React.Component {
   constructor (props) {
     super(props)
@@ -31,13 +25,15 @@ class TeamForm extends React.Component {
     return (
       <div>
       <div className="input-field">
-        <label htmlFor="name">Team Name</label>
-        <input id='name' value={this.state.name} onChange={this.handleNameChange} />
+        <input id='name' value={this.state.name} onChange={this.handleNameChange} placeholder='Team Name'/>
       </div>
         <input
+          className='btn green'
           type="submit"
           onClick={() =>
-            db.createTeam(this.state.name)
+            db.createTeam(this.state.name).then(teamId =>
+              Router.push(`/manageTeam?id=${teamId}`)
+            )
           }
         />
       </div>
